bb0739425a59a4b2197573cbb15e3389846287ff
deathknight13579@aim.com <deathknight13579@aim.com@811ab3ee-a899-11dd-9a07-0f3041a8e97c>
2010-02-17 09:28:51 +0000

    Added Battle.net filtered words.

---
+++ trunk/data/Blizzard/Starcraft/scenums3.h
@@ -0,0 +1,29 @@
+enum WPN_EffectTypes
+{
+  WPNEffect_None,
+  WPNEffect_Normal,
+  WPNEffect_Splash_Radial,
+  WPNEffect_Splash_Enemy,
+  WPNEffect_Lockdown,
+  WPNEffect_NuclearMissile,
+  WPNEffect_Parasite,
+  WPNEffect_Broodlings,
+  WPNEffect_EMPShockwave,
+  WPNEffect_Irradiate,
+  WPNEffect_Ensnare,
+  WPNEffect_Plague,
+  WPNEffect_StasisField,
+  WPNEffect_DarkSwarm,
+  WPNEffect_Consume,
+  WPNEffect_YamatoGun,
+  WPNEffect_Restoration,
+  WPNEffect_DisruptionWeb,
+  WPNEffect_CorrosiveAcid,
+  WPNEffect_MindControl,
+  WPNEffect_Feedback,
+  WPNEffect_OpticalFlare,
+  WPNEffect_Maelstrom,
+  WPNEffect_Unknown,
+  WPNEffect_Splash_Air
+};
+
